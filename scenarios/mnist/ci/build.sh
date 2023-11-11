8b4c55017faa7f936de2353b7346e3069b3f7d01
avssridhar <avssridhar111@gmail.com>
2023-11-11 20:39:29 +0530

    more changes

--- scenarios/mnist/ci/build.sh
+++ scenarios/mnist/ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f ci/Dockerfile.icmr src -t preprocess-icmr:latest
-docker build -f ci/Dockerfile.index src -t preprocess-index:latest
-docker build -f ci/Dockerfile.cowin src -t preprocess-cowin:latest
+docker build -f ci/Dockerfile.mnist_1 src -t preprocess-mnist_1:latest
+docker build -f ci/Dockerfile.mnist_2 src -t preprocess-mnist_2:latest
+docker build -f ci/Dockerfile.mnist_3 src -t preprocess-mnist_3:latest
 docker build -f ci/Dockerfile.modelsave src -t ccr-model-save:latest
