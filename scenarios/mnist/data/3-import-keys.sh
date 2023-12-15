44d7d5223e3304e50b9ea0d5dab0124988276012
A.V.S.SRIDHAR <avssridhar111@gmail.com>
2023-12-15 22:18:27 +0530

    Update 3-import-keys.sh
    making the KID consistent

--- scenarios/mnist/data/3-import-keys.sh
+++ scenarios/mnist/data/3-import-keys.sh
@@ -45,9 +45,9 @@ elif [[ "$AZURE_KEYVAULT_ENDPOINT" == *".managedhsm.azure.net" ]]; then
 fi
 
 DATADIR=`pwd`
-import_key "MNIST_1FilesystemEncryptionKey" $DATADIR/mnist_1key.bin
-import_key "MNIST_2FilesystemEncryptionKey" $DATADIR/mnist_2key.bin
-import_key "MNIST_3FilesystemEncryptionKey" $DATADIR/mnist_3key.bin
+import_key "MNIST_1_FilesystemEncryptionKey" $DATADIR/mnist_1key.bin
+import_key "MNIST_2_FilesystemEncryptionKey" $DATADIR/mnist_2key.bin
+import_key "MNIST_3_FilesystemEncryptionKey" $DATADIR/mnist_3key.bin
 import_key "ModelFilesystemEncryptionKey" $DATADIR/modelkey.bin
 import_key "OutputFilesystemEncryptionKey" $DATADIR/outputkey.bin
 
