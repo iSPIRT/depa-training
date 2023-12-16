ef7cab393276edf21f8d3a2a6b08a2415497c3e0
A.V.S.SRIDHAR <avssridhar111@gmail.com>
2023-12-16 11:53:19 +0530

    Update 4-encrypt-data.sh
    changed names

--- scenarios/mnist/data/4-encrypt-data.sh
+++ scenarios/mnist/data/4-encrypt-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./generatefs.sh -d icmr/preprocessed -k icmrkey.bin -i icmr.img
-./generatefs.sh -d cowin/preprocessed -k cowinkey.bin -i cowin.img
-./generatefs.sh -d index/preprocessed -k indexkey.bin -i index.img
+./generatefs.sh -d mnist_1/preprocessed -k mnist_1_key.bin -i mnist_1.img
+./generatefs.sh -d mnist_2/preprocessed -k mnist_2_key.bin -i mnist_2.img
+./generatefs.sh -d mnist_3/preprocessed -k mnist_3_key.bin -i mnist_3.img
 ./generatefs.sh -d modeller/model -k modelkey.bin -i model.img
 mkdir -p output
 ./generatefs.sh -d output -k outputkey.bin -i output.img
