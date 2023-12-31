22d7a464d4a0462ef16723de4b4fc0b70861e0d2
Kapil Vaswani <kapilv@microsoft.com>
2023-12-31 16:51:22 +0000

    mnist logic

---
+++ scenarios/mnist/data/3-import-keys.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Function to import a key with a given key ID and key material into AKV
+# The key is bound to a key release policy with host data defined in the environment variable CCE_POLICY_HASH
+function import_key() {
+  export KEYID=$1
+  export KEYFILE=$2
+
+  # For RSA-HSM keys, we need to set a salt and label which will be used in the symmetric key derivation
+  if [ "$AZURE_AKV_KEY_TYPE" = "RSA-HSM" ]; then    
+    export AZURE_AKV_KEY_DERIVATION_LABEL=$KEYID
+  fi
+
+  CONFIG=$(jq '.claims[0][0].equals = env.CCE_POLICY_HASH' importkey-config-template.json)
+  CONFIG=$(echo $CONFIG | jq '.key.kid = env.KEYID')
+  CONFIG=$(echo $CONFIG | jq '.key.kty = env.AZURE_AKV_KEY_TYPE')
+  CONFIG=$(echo $CONFIG | jq '.key_derivation.salt = "9b53cddbe5b78a0b912a8f05f341bcd4dd839ea85d26a08efaef13e696d999f4"')
+  CONFIG=$(echo $CONFIG | jq '.key_derivation.label = env.AZURE_AKV_KEY_DERIVATION_LABEL')
+  CONFIG=$(echo $CONFIG | jq '.key.akv.endpoint = env.AZURE_KEYVAULT_ENDPOINT')
+  CONFIG=$(echo $CONFIG | jq '.key.akv.bearer_token = env.BEARER_TOKEN')
+  echo $CONFIG > /tmp/importkey-config.json
+  echo "Importing $KEYID key with key release policy"
+  jq '.key.akv.bearer_token = "REDACTED"' /tmp/importkey-config.json
+  pushd . && cd $TOOLS_HOME/importkey && go run main.go -c /tmp/importkey-config.json -out && popd
+  mv $TOOLS_HOME/importkey/keyfile.bin $KEYFILE
+}
+
+echo Obtaining contract service parameters...
+CONTRACT_SERVICE_URL=${CONTRACT_SERVICE_URL:-"http://localhost:8000"}
+export CONTRACT_SERVICE_PARAMETERS=$(curl -k -f $CONTRACT_SERVICE_URL/parameters | base64 --wrap=0)
+
+envsubst < ../policy/policy-in-template.json > /tmp/policy-in.json
+export CCE_POLICY=$(az confcom acipolicygen -i /tmp/policy-in.json --debug-mode)
+export CCE_POLICY_HASH=$(go run $TOOLS_HOME/securitypolicydigest/main.go -p $CCE_POLICY)
+echo "Training container policy hash $CCE_POLICY_HASH"
+
+# Obtain the token based on the AKV resource endpoint subdomain
+if [[ "$AZURE_KEYVAULT_ENDPOINT" == *".vault.azure.net" ]]; then
+    export BEARER_TOKEN=$(az account get-access-token --resource https://vault.azure.net | jq -r .accessToken)
+    echo "Importing keys to AKV key vaults can be only of type RSA-HSM"
+    export AZURE_AKV_KEY_TYPE="RSA-HSM"
+elif [[ "$AZURE_KEYVAULT_ENDPOINT" == *".managedhsm.azure.net" ]]; then
+    export BEARER_TOKEN=$(az account get-access-token --resource https://managedhsm.azure.net | jq -r .accessToken)    
+    export AZURE_AKV_KEY_TYPE="oct-HSM"
+fi
+
+DATADIR=`pwd`
+import_key "MNISTFilesystemEncryptionKey" $DATADIR/mnistkey.bin
+import_key "ModelFilesystemEncryptionKey" $DATADIR/modelkey.bin
+import_key "OutputFilesystemEncryptionKey" $DATADIR/outputkey.bin
+
+## Cleanup
+rm /tmp/importkey-config.json
+rm /tmp/policy-in.json
--- scenarios/mnist/deployment/docker/preprocess.sh
+++ scenarios/mnist/deployment/docker/preprocess.sh
@@ -1,4 +1,5 @@
 export DATA_DIR=$PWD/../../data
 export MNIST_INPUT_PATH=$DATA_DIR
-export MNIST_OUTPUT_PATH=$DATA_DIR
+export MNIST_OUTPUT_PATH=$DATA_DIR/preprocessed
+mkdir -p $MNIST_OUTPUT_PATH
 docker compose -f docker-compose-preprocess.yml up --remove-orphans
