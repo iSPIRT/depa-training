3249c7a5f2dabd513b6b365f8cfb9208c2b7c742
Sarang Galada <32020201+saranggalada@users.noreply.github.com>
2025-07-25 23:36:26 +0530

    Update load_base_model.py

--- scenarios/mri-tumor-segmentation/src/load_base_model.py
+++ scenarios/mri-tumor-segmentation/src/load_base_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import onnx
+# import onnx
 
 # Model architecture and components for Anatomy UNet
 class ConvBlock2d(nn.Module):
